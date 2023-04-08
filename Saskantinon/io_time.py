0e11860d140e683cea78daadc83fa22f59247d06
PQ <pq_rfw@pm.me>
2023-04-08 15:56:53 -0400

    Refactor time and geo class

--- Saskantinon/io_time.py
+++ Saskantinon/io_time.py
@@ -96,12 +96,13 @@ class TimeIO(object):
         PCT = "percent"
         RD = "radius"
         VL = "volume"
-        # weight, mass
-        BA = "baryonic"
+        # weight
         GM = "grams"
         KG = "kilograms"
         LB = "pounds"
         OZ = "ounces"
+        # mass
+        BA = "baryonic"
         MS = "mass"
         SM = "solar mass"
         # distance & area, metric, imperial, saskan
@@ -149,11 +150,6 @@ class TimeIO(object):
         """
         # math, geometry, currency
         # Fill in using real and game currenncies
-        # weight, mass - metric/imperial
-        KG_TO_LB = 2.20462262185     # kilos -> pounds
-        KG_TO_SM = 5.97219e-31       # kilos -> solar mass
-        LB_TO_KG = 0.45359237        # pounds -> kilos
-        SM_TO_KG = 1.98847e+30       # solar mass -> kilos
         # distance - metric/imperial
         CM_TO_IN = 0.3937007874      # centimeters -> inches
         CM_TO_M = 0.01               # centimeters -> meters
@@ -550,14 +546,14 @@ class TimeIO(object):
         """Convert decimal to percentage.
         """
         dec = float(p_decimal)
-        pct = f'{round(dec * 100, 2):,}' + " %"
+        pct = f'{round(dec * 100, 5):,}' + " %"
         return pct
 
     def pct_to_dec(self, p_percent: str) -> float:
         """Convert percentage to decimal factor.
         """
         pct_s = str(p_percent).replace("%", "").strip()
-        pct_d = float(pct_s) / 100
+        pct_d = round(float(pct_s) / 100, 5)
         return pct_d
 
     def diam_to_radius(self, p_diameter: float) -> float:
@@ -569,43 +565,47 @@ class TimeIO(object):
     def radius_to_diam(self, p_radius: float) -> float:
         """Convert radius to diameter.
         """
-        diam = float(p_radius) * 2
+        diam = round(float(p_radius) * 2, 5)
         return diam
 
     def grams_to_kilos(self,
-                       p_grams: float) -> float:
+                       p_grams: float,
+                       p_rounding: bool = True) -> float:
         """Convert grams to kilos."""
-        kilos = float(p_grams) * 0.001
+        if p_rounding:
+            kilos = round(float(p_grams) * 0.001, 5)
+        else:
+            kilos = float(p_grams) * 0.001
         return kilos
 
     def kilos_to_grams(self,
                        p_kilos: float) -> float:
         """Convert kilos to grams."""
-        grams = float(p_kilos) * 1000
+        grams = round(float(p_kilos) * 1000, 5)
         return grams
 
     def kilos_to_pounds(self,
                         p_kilos: float) -> float:
         """Convert kilos to pounds."""
-        pounds = float(p_kilos) * 2.20462262185
+        pounds = round(float(p_kilos) * 2.20462262185, 5)
         return pounds
 
     def pounds_to_kilos(self,
                         p_pounds: float) -> float:
         """Convert pounds to kilos."""
-        kilos = float(p_pounds) * 0.45359237
+        kilos = round(float(p_pounds) * 0.45359237, 5)
         return kilos
 
     def pounds_to_oz(self,
                      p_pounds: float) -> float:
         """Convert pounds to ounces."""
-        oz = float(p_pounds) * 16
+        oz = round(float(p_pounds) * 16, 5)
         return oz
 
     def oz_to_pounds(self,
                      p_oz: float) -> float:
         """Convert ounces to pounds."""
-        pounds = float(p_oz) / 16
+        pounds = round(float(p_oz) / 16, 5)
         return pounds
 
     def oz_to_grams(self,
@@ -617,20 +617,118 @@ class TimeIO(object):
     def grams_to_oz(self,
                     p_grams: float) -> float:
         """Convert grams to ounces."""
-        oz = float(p_grams) * 0.03527396195
+        oz = round(float(p_grams) * 0.03527396195, 5)
         return oz
 
     def kilos_to_sm(self,
-                    p_kilos: float) -> float:
+                    p_kilos: float,
+                    p_rounding: bool = True) -> float:
         """Convert kilos to solar mass."""
-        sm = float(p_kilos) * 5.97219e-31
+        if p_rounding:
+            sm = round(float(p_kilos) * 5.97219e-31, 5)
+        else:
+            sm = float(p_kilos) * 5.97219e-31
         return sm
 
     def sm_to_kilos(self,
                     p_sm: float) -> float:
         """Convert solar mass to kilos."""
-        kilos = float(p_sm) * 1.98847e+30
+        kilos = round(float(p_sm) * 1.98847e+30, 5)
         return kilos
+
+    # CM_TO_IN = 0.3937007874      # centimeters -> inches
+    # IN_TO_CM = 2.54              # inches -> centimeters
+    def cm_to_inches(self,
+                     p_cm: float) -> float:
+        """Convert centimeters to inches."""
+        inches = round(float(p_cm) * 0.3937007874, 5)
+        return inches
+
+    def inches_to_cm(self,
+                        p_inches: float) -> float:
+        """Convert inches to centimeters."""
+        cm = round(float(p_inches) * 2.54, 5)
+        return cm
+
+    # FT_TO_M = 0.3048             # feet -> meters
+    # M_TO_FT = 3.280839895        # meters -> feet
+
+    def ft_to_meters(self,
+                     p_ft: float) -> float:
+        """Convert feet to meters."""
+        meters = round(float(p_ft) * 0.3048, 5)
+        return meters
+
+    def meters_to_ft(self,
+                     p_meters: float) -> float:
+        """Convert meters to feet."""
+        ft = round(float(p_meters) * 3.280839895, 5)
+        return ft
+
+        # FT_TO_IN = 12.0              # feet -> inches
+        # IN_TO_FT = 0.08333333333     # inches -> feet
+        # IN_TO_MM = 25.4              # inches -> millimeters
+        # MM_TO_IN = 0.03937007874     # millimeters -> inches
+
+    def ft_to_inches(self,
+                     p_ft: float) -> float:
+        """Convert feet to inches."""
+        inches = round(float(p_ft) * 12, 5)
+        return inches
+
+    def inches_to_ft(self,
+                     p_inches: float,
+                     p_rounding: bool=True) -> float:
+        """Convert inches to feet."""
+        if p_rounding:
+            ft = round(float(p_inches) * 0.08333333333, 5)
+        else:
+            ft = float(p_inches) * 0.08333333333
+        return ft
+
+        # CM_TO_M = 0.01               # centimeters -> meters
+        # M_TO_CM = 100.0              # meters -> centimeters
+        # CM_TO_MM = 10.0              # centimeters -> millimeters
+        # MM_TO_CM = 0.1               # millimeters -> centimeters
+
+        # KM_TO_M = 1000.0             # kilometers -> meters
+        # M_TO_KM = 0.001              # meters -> kilometers
+
+        # MI_TO_NM = 0.868976242       # miles -> nautical miles
+        # NM_TO_MI = 1.150779448       # nautical miles -> miles
+        # KM_TO_MI = 0.62137119223733  # kilometers -> miles
+        # MI_TO_KM = 1.609344          # miles -> kilometers
+        # KM_TO_NM = 0.539956803       # kilometers -> nautical miles
+        # NM_TO_KM = 1.852             # nautical miles -> kilometers
+
+        # distance - saskan/metric
+        # come up with something smaller than a nob, say, a pik
+        # CM_TO_NOB = 0.64             # centimeters -> nobs
+        # GABO_TO_MI = 0.636           # gabos -> miles
+        # GAWO_TO_KATA = 4.0           # gawos -> kata
+        # GAWO_TO_KM = 1.024           # gawos -> kilometers
+        # GAWO_TO_M = 1024.0           # gawos -> meters
+        # IN_TO_NOB = 2.56             # inches -> nobs
+        # KATA_TO_KM = 0.256           # kata -> kilometers
+        # KATA_TO_M = 256.0            # kata -> meters
+        # KATA_TO_MI = 0.159           # ktaa -> miles
+        # KATA_TO_THWAB = 4.0          # kata -> thwabs
+        # M_TO_NOB = 64.0              # meters -> nobs
+        # M_TO_THWAB = 0.015625        # meters -> thwabs (1/64th)
+        # MM_TO_NOB = 0.0064           # millimeters -> nobs
+        # NOB_TO_CM = 1.5625           # nobs -> centimeters
+        # NOB_TO_IN = 0.390625         # nobs -> inches
+        # NOB_TO_MM = 156.25           # nobs -> millimeters
+        # THWAB_TO_KATA = 0.25         # thwabs -> kata
+        # THWAB_TO_M = 64.0            # thwabs -> meters
+        # THWAB_TO_TWA = 64.0          # thwabs -> twas
+        # TWA_TO_M = 1.00              # twas -> meters
+        # TWA_TO_NOB = 64.0            # twas -> nobs
+        # TWA_TO_THWAB = 0.015625      # twas -> thwabs (1/64th)
+        # YUZA_TO_GABO = 4.0           # yuzas -> gabos
+        # YUZA_TO_KM = 4.096           # yuzas -> kilometers
+        # YUZA_TO_M = 4096.0           # yuzas -> meters
+        # YUZA_TO_MI = 2.545           # yuzas -> miles
 
     def get_map_dim(self,
                     p_map_w: float,
--- Saskantinon/io_time_test.py
+++ Saskantinon/io_time_test.py
@@ -50,9 +50,12 @@ class TimeIOTest(object):
     def test_weight_and_mass(self):
         """Test weight and mass computations.
         """
-        print(f"{TI.M.GM} to {TI.M.KG}\n==================")
+        print(f"{TI.M.GM} to {TI.M.KG} default rounding\n==================")
         for g in (0, 5e+10, 3e-3, 27.34, 84734678.2, 5.6e-87):
             print(g, "-->", TI.grams_to_kilos(g))
+        print(f"\n{TI.M.GM} to {TI.M.KG} no rounding\n==================")
+        for g in (0, 5e+10, 3e-3, 27.34, 84734678.2, 5.6e-87):
+            print(g, "-->", TI.grams_to_kilos(g, p_rounding=False))
         print(f"\n{TI.M.KG} to {TI.M.GM}\n==================")
         for k in (0, 5e+10, 3e-3, 27.34, 84734678.2, 5.6e+86):
             print(k, "-->", TI.kilos_to_grams(k))
@@ -74,13 +77,40 @@ class TimeIOTest(object):
         print(f"\n{TI.M.GM} to {TI.M.OZ}\n==================")
         for g in (100, 25, 10, 0.05, 1, 32, 64, 2.7e-32, 10):
             print(g, "-->", TI.grams_to_oz(g))
-        print(f"\n{TI.M.KG} to {TI.M.SM}\n================")
+        print(f"\n{TI.M.KG} to {TI.M.SM} default rounding\n================")
         for k in (100000, 2.5e+10, 2.7e+32, 10.3e+31, 3.8e+30):
             print(k, "-->", TI.kilos_to_sm(k))
+        print(f"\n{TI.M.KG} to {TI.M.SM} no rounding\n================")
+        for k in (100000, 2.5e+10, 2.7e+32, 10.3e+31, 3.8e+30):
+            print(k, "-->", TI.kilos_to_sm(k, p_rounding=False))
         print(f"\n{TI.M.SM} to {TI.M.KG}\n==================")
         for s in (1, 2.5e-10, 2.7e-32, 10.3e-31, 3.8e-30):
             print(s, "-->", TI.sm_to_kilos(s))
 
+    def test_lengths_earth(self):
+        """Test length computations using earthly units.
+        """
+        print(f"{TI.M.CM} to {TI.M.IN}\n==================")
+        for c in (0, 1, 10, 2.54, 100, 12.432, 2.5):
+            print(c, "-->", TI.cm_to_inches(c))
+        print(f"\n{TI.M.IN} to {TI.M.CM}\n==================")
+        for i in (0, 1, 10, 0.3937007874, 100, 12.432, 2.5):
+            print(i, "-->", TI.inches_to_cm(i))
+        print(f"\n{TI.M.FT} to {TI.M.M}\n==================")
+        for f in (0, 1, 3.280839895, 100, 12.5, 3.7e+5):
+            print(f, "-->", TI.ft_to_meters(f))
+        print(f"\n{TI.M.M} to {TI.M.FT}\n==================")
+        for m in (0, 1, 0.3048, 0.1524, 27.9, 2.8e+5):
+            print(m, "-->", TI.meters_to_ft(m))
+        print(f"\n{TI.M.FT} to {TI.M.IN}\n==================")
+        for f in (0, 1, 1/12, 3, 0.5):
+            print(f, "-->", TI.ft_to_inches(f))
+        print(f"\n{TI.M.IN} to {TI.M.FT} w/rounding \n==================")
+        for i in (0, 1, 12, 36, 0.00125, 1.7e-12):
+            print(i, "-->", TI.inches_to_ft(i))
+        print(f"\n{TI.M.IN} to {TI.M.FT} w/o rounding \n==================")
+        for i in (0, 1, 12, 36, 0.00125, 1.7e-12):
+            print(i, "-->", TI.inches_to_ft(i, p_rounding=False))
 
 # Run program
 if __name__ == '__main__':
@@ -88,5 +118,7 @@ if __name__ == '__main__':
     TIT = TimeIOTest()
     # TIT.test_percents()
     # TIT.test_geometry()
-    TIT.test_weight_and_mass()
+    # TIT.test_weight_and_mass()
+    TIT.test_lengths_earth()
+
 
